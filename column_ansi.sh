b7b96634c3a03af2dfa1b80e33ca5c1e0a62ac87
Luca Salvarani <33452387+LukeSavefrogs@users.noreply.github.com>
2021-12-29 14:34:44 +0100

    Added option to center align text
    Now with `-C` or `--table-center` it is possible to align certain columns to the center. Right alignment has precedence in case of conflict.

--- column_ansi.sh
+++ column_ansi.sh
@@ -12,6 +12,7 @@ column_ansi () {
 			local _input_separator=" ";
 			local _output_separator="  ";
 			local _align_right="  ";
+			local _align_center="  ";
 
 			# From this excellent StackOverflow answer: https://stackoverflow.com/a/14203146/8965861
 			OPTIND=1;
@@ -28,6 +29,10 @@ column_ansi () {
 					;;
 					-R | --table-right)
 						_align_right="$2";
+						shift 2;
+					;;
+					-C | --table-center)
+						_align_center="$2";
 						shift 2;
 					;;
 					-t | --table)     # Does nothing - Only for compatibility reasons
@@ -48,6 +53,7 @@ column_ansi () {
 			export PCOLUMN_INPUT_SEPARATOR="${_input_separator}";
 			export PCOLUMN_OUTPUT_SEPARATOR="${_output_separator}";
 			export PCOLUMN_ALIGN_RIGHT="${_align_right}";
+			export PCOLUMN_ALIGN_CENTER="${_align_center}";
 
 			perl -e '
 				use strict;
@@ -72,6 +78,7 @@ column_ansi () {
 				my $INPUT_SEPARATOR = $ENV{"PCOLUMN_INPUT_SEPARATOR"};
 				my $OUTPUT_SEPARATOR = $ENV{"PCOLUMN_OUTPUT_SEPARATOR"};
 				my $ALIGN_RIGHT = $ENV{"PCOLUMN_ALIGN_RIGHT"};
+				my $ALIGN_CENTER = $ENV{"PCOLUMN_ALIGN_CENTER"};
 
 				# Default values for INPUT_SEPARATOR and OUTPUT_SEPARATOR
 				if ($INPUT_SEPARATOR eq ""){
@@ -82,15 +89,31 @@ column_ansi () {
 				}
 
 				# ALIGN_RIGHT must be a single number or a comma-separated list of numbers
+				# Then split ALIGN_RIGHT into an array and then convert it to an hash that will be used to check
+				#   if the column needs to be aligned to the right
 				$ALIGN_RIGHT =~ s/^\s+|\s+$//g;
 				if ($ALIGN_RIGHT ne "" && not $ALIGN_RIGHT =~ /^[0-9]+(,[0-9]+)*$/) {
 					print STDERR "error: undefined column name '\''$ALIGN_RIGHT'\''\n";
 					exit 1;
 				}
-				my @ALIGN_RIGHT = split(/,/, $ALIGN_RIGHT);
-				my %ALIGN_RIGHT_HASH = map {$_ - 1 => 1} @ALIGN_RIGHT;
+				my %ALIGN_RIGHT_HASH = map {$_ - 1 => 1} split(/,/, $ALIGN_RIGHT);
+				
+				
+				# ALIGN_CENTER must be a single number or a comma-separated list of numbers
+				# Then split ALIGN_CENTER into an array and then convert it to an hash that will be used to check
+				#   if the column needs to be aligned to the right
+				$ALIGN_CENTER =~ s/^\s+|\s+$//g;
+				if ($ALIGN_CENTER ne "" && not $ALIGN_CENTER =~ /^[0-9]+(,[0-9]+)*$/) {
+					print STDERR "error: undefined column name '\''$ALIGN_CENTER'\''\n";
+					exit 1;
+				}
+				my %ALIGN_CENTER_HASH = map {$_ - 1 => 1} split(/,/, $ALIGN_CENTER);
+
+				# Save the STDIN into an Array, so that we can loop over it multiple times
 				my @stdin = <STDIN>;
 				my $column_widths = [];
+
+				# First loop: get the width of each column and save it in an array
 				foreach my $line (@stdin) {
 					$line =~ s/\r?\n?$//;
 					my @columns = split(/\Q$INPUT_SEPARATOR/, $line);
@@ -109,20 +132,31 @@ column_ansi () {
 						$column_index++;
 					}
 				}
+
+				# Second loop: print the columns
 				foreach my $line (@stdin) {
 					$line =~ s/\r?\n?$//;
 					my @columns = split (/\Q$INPUT_SEPARATOR/, $line);
 					my $column_index = 0;
+
 					foreach my $column (@columns) {
 						my $__current_column_length = length(trim_ansi($column));
 						my $__current_column_padding = $column_widths->[$column_index] - $__current_column_length;
+						
 						if (exists $ALIGN_RIGHT_HASH{$column_index}) {
 							print(" " x $__current_column_padding);
 							print($column);
+						} elsif (exists $ALIGN_CENTER_HASH{$column_index}) {
+							my $__left_padding = int($__current_column_padding / 2);
+							my $__right_padding = $__current_column_padding - $__left_padding;
+							print(" " x $__left_padding);
+							print($column);
+							print(" " x $__right_padding);
 						} else {
 							print($column);
 							print(" " x $__current_column_padding);
 						}
+						
 						if ($column_index != $#columns) {
 							print($OUTPUT_SEPARATOR);
 						}
@@ -136,6 +170,7 @@ column_ansi () {
 			unset PCOLUMN_INPUT_SEPARATOR;
 			unset PCOLUMN_OUTPUT_SEPARATOR;
 			unset PCOLUMN_ALIGN_RIGHT;
+			unset PCOLUMN_ALIGN_CENTER;
 		}
 
 		# shellckeck disable=SC2059
@@ -161,17 +196,20 @@ column_ansi () {
 			printf "\n";
 			
 			printf "${__bold}OPTIONS${__reset}:\n";
+			printf "${__indent_1}${__red}-t${__reset}, ${__red}--table${__reset}\n";
+			printf "${__indent_2}Does nothing, was left for compatibility reasons. [DEFAULT]\n";
+			printf "\n";
 			printf "${__indent_1}${__red}-s${__reset} ${__underlined}SEPARATOR${__reset}, ${__red}--separator${__reset} ${__underlined}SEPARATOR${__reset}\n";
 			printf "${__indent_2}Specify the possible input item delimiters (default is whitespace).\n";
 			printf "\n";
 			printf "${__indent_1}${__red}-r${__reset} ${__underlined}SEPARATOR${__reset}, ${__red}--output-separator${__reset} ${__underlined}SEPARATOR${__reset}\n";
 			printf "${__indent_2}Specify the columns delimiter for table output (default is two spaces).\n";
 			printf "\n";
-			printf "${__indent_1}${__red}-t${__reset}, ${__red}--table${__reset}\n";
-			printf "${__indent_2}Does nothing, was left for compatibility reasons. [DEFAULT]\n";
-			printf "\n";
 			printf "${__indent_1}${__red}-R${__reset} ${__underlined}COLUMNS${__reset}, ${__red}--table-right${__reset} ${__underlined}COLUMNS${__reset}\n";
 			printf "${__indent_2}Right align text in the specified columns (comma-separated).\n";
+			printf "\n";
+			printf "${__indent_1}${__red}-C${__reset} ${__underlined}COLUMNS${__reset}, ${__red}--table-center${__reset} ${__underlined}COLUMNS${__reset}\n";
+			printf "${__indent_2}Center align text in the specified columns (comma-separated).\n";
 			printf "\n";
 			printf "${__indent_1}${__red}-h${__reset}, ${__red}--help${__reset}\n";
 			printf "${__indent_2}Display help text and exit.\n";
